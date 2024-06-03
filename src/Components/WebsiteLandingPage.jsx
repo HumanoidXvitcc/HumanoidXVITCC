a9cd6a942fcfb0d744564f9756a3a80b61d085c0
Spacecentre <akkilalagar05@gmail.com>
2024-06-03 13:30:37 +0530

    updated OBS 24-25

--- src/Components/WebsiteLandingPage.jsx
+++ src/Components/WebsiteLandingPage.jsx
@@ -17,8 +17,8 @@ const WebsiteLandingPage = () => {
             <VideoPlayer />
             <ImageCarousel />
             {/* <SynthetixBanner />  */}
-            <Footer />   
             <Obs />   
+            <Footer />   
         </div>
     </div>
   )
